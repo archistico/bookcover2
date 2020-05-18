937d20e178dbf7fdd5d47d5956cf872f4be5f8a5
unknown <info@archistico.com>
2020-05-18 21:37:43 +0200

    add tailwind css

--- index.php
+++ index.php
@@ -1,16 +1,70 @@
-<?php
-require_once(__DIR__ . '/vendor/autoload.php');
-require_once(__DIR__ . '/constant.php');
+<!DOCTYPE html>
+<html lang="en">
 
-use \App\Punto;
-use \App\Copertina;
-use \App\Dorso;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="flex flex-wrap">
+        <div class="w-1/3 ml-auto mr-auto">
+
+            <form class="w-full max-w-sm">
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                            Full Name
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe">
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-username">
+                            Password
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-username" type="password" placeholder="******************">
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-username">
+                            Password
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <div class="inline-block relative w-64">
+                            <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                <option>New Mexico</option>
+                                <option>Missouri</option>
+                                <option>Texas</option>
+                            </select>
+                            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="md:flex md:items-center">
+                    <div class="md:w-1/3"></div>
+                    <div class="md:w-2/3">
+                        <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                            Sign Up
+                        </button>
+                    </div>
+                </div>
+            </form>
 
 
-$cover = new Copertina(150, 210, 5, 3, 0, 10, COPERTINA_RILEGATURA_FRESATA, 104, COPERTINA_GRAMMATURA_100);
-$cover->setTitolo('TITOLO')
-        ->setAutore('AUTORE')
-        ->setPrezzo(9.99)
-        ->setISBN('978-88-12345-00-1');
-        
-$cover->Draw();
+        </div>
+    </div>
+
+</html>
